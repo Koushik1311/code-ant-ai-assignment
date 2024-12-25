52b3de6afe16fa3daa249f98af38486789f858aa
Koushik <programmer.koushik@gmail.com>
2024-12-25 16:39:06 +0530

    style: change max width

--- components/global/MaxWidthWrapper.tsx
+++ components/global/MaxWidthWrapper.tsx
@@ -12,7 +12,7 @@ export default function MaxWidthWrapper({
   return (
     <div
       className={cn(
-        "h-full mx-auto max-w-screen-xl px-2.5 md:px-20",
+        "h-full mx-auto max-w-screen-2xl px-2.5 md:px-20",
         className
       )}
     >
